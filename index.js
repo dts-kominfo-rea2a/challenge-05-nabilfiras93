38d9259e670689e330efc9f84f90871d792510d0
BIlnabil <nabilfiras93@gmail.com>
2022-06-05 20:12:10 +0700

    09 - JS Callback - Tantangan

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrName, fnSort)=>{
+  let result_ = [];
+  
+  if(fnSort(arrName)){
+    let inc=1
+    for(counter=0;counter<arrName.length;counter++){
+      result_.push(inc++ + ". " +arrName[counter])
+    }
+  }
+  return result_;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  return arr.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  return arr.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
